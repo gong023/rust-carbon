5173b12d3267196b3b38ea7d6836723080c6d8d6
gong023 <gon.gong.gone@gmail.com>
2015-06-16 00:59:39 +0900

    refactor start

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 extern crate time;
 
+mod start;
+
 pub use time::*;
+pub use start::Start;
 
 pub struct DateTime {
     pub t: Tm,
@@ -19,59 +22,8 @@ impl DateTime {
         }
     }
 
-    pub fn to_string(&self) -> time::TmFmt {
-        self.t.ctime()
-    }
-
-    pub fn unixtime(&self) -> time::Timespec {
-        self.t.to_timespec()
-    }
-
-    pub fn start_of_month(&self) -> DateTime {
-        let mut copied_tm = self.t;
-        copied_tm.tm_mday = 1;
-
-        match self.t.tm_mday % 7 {
-            0 =>  copied_tm.tm_wday = 0,
-            _ => { copied_tm.tm_wday = self.t.tm_wday - (self.t.tm_mday % 7) + 1; },
-        }
-
-        match self.t.tm_mon {
-            1 => copied_tm.tm_yday = 0,
-            _ => {
-                let mut yday = 0;
-                for m in (1..self.t.tm_mon) {
-                    yday += self.days_in_month(m);
-                }
-                copied_tm.tm_yday = yday + 1;
-            }
-        }
-
-        DateTime::create_from_tm(copied_tm).start_of_day()
-    }
-
-    pub fn start_of_day(&self) -> DateTime {
-        let mut copied_tm = self.t;
-        copied_tm.tm_hour = 0;
-        DateTime::create_from_tm(copied_tm).start_of_hour()
-    }
-
-    pub fn start_of_hour(&self) -> DateTime {
-        let mut copied_tm = self.t;
-        copied_tm.tm_min = 0;
-        DateTime::create_from_tm(copied_tm).start_of_minute()
-    }
-
-    pub fn start_of_minute(&self) -> DateTime {
-        let mut copied_tm = self.t;
-        copied_tm.tm_sec = 0;
-        DateTime::create_from_tm(copied_tm).start_of_second()
-    }
-
-    pub fn start_of_second(&self) -> DateTime {
-        let mut copied_tm = self.t;
-        copied_tm.tm_nsec = 0;
-        DateTime::create_from_tm(copied_tm)
+    pub fn start_of(&self) -> Start {
+        Start::new(&self)
     }
 
     pub fn is_leap_year(&self) -> bool {
@@ -79,13 +31,11 @@ impl DateTime {
         (y % 4 == 0) && ((y % 100 != 0) || (y % 400 == 0))
     }
 
-    fn days_in_month(&self, month: i32) -> i32 {
-        match month {
-            2 => { if self.is_leap_year() { 29 } else { 28 } },
-            4 | 6 | 9 | 11 => 31,
-            _ => 30,
-        }
+    pub fn to_string(&self) -> time::TmFmt {
+        self.t.ctime()
+    }
+
+    pub fn unixtime(&self) -> time::Timespec {
+        self.t.to_timespec()
     }
 }
-
-
---
+++ src/start.rs
@@ -0,0 +1,84 @@
+pub use time::*;
+
+use super::DateTime;
+
+pub struct Start<'a> {
+    date_time: &'a DateTime
+}
+
+trait Hoge {
+    fn month(&self) -> DateTime;
+
+    fn day(&self) -> DateTime;
+
+    fn hour(&self) -> DateTime;
+
+    fn minute(&self) -> DateTime;
+
+    fn second(&self) -> DateTime;
+}
+
+impl<'a> Start<'a> {
+    pub fn new(dt: &'a DateTime) -> Start {
+        Start {
+            date_time: dt
+        }
+    }
+
+    fn days_in_month(&self, month: i32) -> i32 {
+        match month {
+            2 => { if self.date_time.is_leap_year() { 29 } else { 28 } },
+            4 | 6 | 9 | 11 => 31,
+            _ => 30,
+        }
+    }
+}
+
+impl<'a> Hoge for Start<'a> {
+    fn month(&self) -> DateTime {
+        let mut copied_tm = self.date_time.t;
+        copied_tm.tm_mday = 1;
+
+        match self.date_time.t.tm_mday % 7 {
+            0 =>  copied_tm.tm_wday = 0,
+            _ => { copied_tm.tm_wday = self.date_time.t.tm_wday - (self.date_time.t.tm_mday % 7) + 1; },
+        }
+
+        match self.date_time.t.tm_mon {
+            1 => copied_tm.tm_yday = 0,
+            _ => {
+                let mut yday = 0;
+                for m in (1..self.date_time.t.tm_mon) {
+                    yday += self.days_in_month(m);
+                }
+                copied_tm.tm_yday = yday + 1;
+            }
+        }
+
+        DateTime::create_from_tm(copied_tm).start_of().day()
+    }
+
+    fn day(&self) -> DateTime {
+        let mut copied_tm = self.date_time.t;
+        copied_tm.tm_hour = 0;
+        DateTime::create_from_tm(copied_tm).start_of().hour()
+    }
+
+    fn hour(&self) -> DateTime {
+        let mut copied_tm = self.date_time.t;
+        copied_tm.tm_min = 0;
+        DateTime::create_from_tm(copied_tm).start_of().minute()
+    }
+
+    fn minute(&self) -> DateTime {
+        let mut copied_tm = self.date_time.t;
+        copied_tm.tm_sec = 0;
+        DateTime::create_from_tm(copied_tm).start_of().second()
+    }
+
+    fn second(&self) -> DateTime {
+        let mut copied_tm = self.date_time.t;
+        copied_tm.tm_nsec = 0;
+        DateTime::create_from_tm(copied_tm)
+    }
+}
